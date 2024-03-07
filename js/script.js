eb7fc61fe5058ebfb619aedcaeec8b6b142dcfef
waheedali6 <waheedalidahri6@gmail.com>
2024-03-07 16:00:28 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function toggleHide() {
+    let body = document.getElementById('body');
+    if (body.style.display != 'block') {
+        body.style.display = 'block';
+        
+    }
+
+    else{
+        body.style.display = 'none';
+    }
+}
